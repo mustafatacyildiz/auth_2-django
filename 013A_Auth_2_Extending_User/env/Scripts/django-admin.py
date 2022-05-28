4706cee856a417af33435ec1798a9b063894af16
Mustafa TAÇYILDIZ <93773148+D1422-Mustafa@users.noreply.github.com>
2022-05-28 13:38:14 +0300

    first commit

---
+++ 013A_Auth_2_Extending_User/env/Scripts/django-admin.py
@@ -0,0 +1,21 @@
+#!C:\Users\Mustafa\Desktop\backend\backend\session_13 auth_2\013A_Auth_2_Extending_User\env\Scripts\python.exe
+# When the django-admin.py deprecation ends, remove this script.
+import warnings
+
+from django.core import management
+
+try:
+    from django.utils.deprecation import RemovedInDjango40Warning
+except ImportError:
+    raise ImportError(
+        'django-admin.py was deprecated in Django 3.1 and removed in Django '
+        '4.0. Please manually remove this script from your virtual environment '
+        'and use django-admin instead.'
+    )
+
+if __name__ == "__main__":
+    warnings.warn(
+        'django-admin.py is deprecated in favor of django-admin.',
+        RemovedInDjango40Warning,
+    )
+    management.execute_from_command_line()
